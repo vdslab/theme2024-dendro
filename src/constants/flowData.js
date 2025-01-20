6a8a82c2184651f3e3fa83b23cb673bf3b13268c
akimas1846 <akihiro0923092@gmail.com>
2025-01-21 03:10:20 +0900

    jsonNameにmaterialFlowDataNameMap変数を追加

--- src/constants/flowData.js
+++ src/constants/flowData.js
@@ -68,3 +68,186 @@ export const peopleFlowDataNameMap = {
     displayName: "営業用乗用車利用数",
   },
 };
+
+export const materialFlowDataNameMap = {
+  totalAll: {
+    id: "totalAll",
+    fileName: "totalAll",
+    displayName: "全機関総貨物数",
+  },
+  agProdAll: {
+    id: "agProdAll",
+    fileName: "agProdAll",
+    displayName: "全機関農林水産品数",
+  },
+  chemicalIndustryProductsAll: {
+    id: "chemicalIndustryProductsAll",
+    fileName: "chemicalIndustryProductsAll",
+    displayName: "全機関化学工業品数",
+  },
+  forestProductsAll: {
+    id: "forestProductsAll",
+    fileName: "forestProductsAll",
+    displayName: "全機関林産品数",
+  },
+  lightIndustryProductsAll: {
+    id: "lightIndustryProductsAll",
+    fileName: "lightIndustryProductsAll",
+    displayName: "全機関軽工業品数",
+  },
+  MandMProductsAll: {
+    id: "MandMProductsAll",
+    fileName: "MandMProductsAll",
+    displayName: "全機関金属機械工業品数",
+  },
+  mineralProductsAll: {
+    id: "mineralProductsAll",
+    fileName: "mineralProductsAll",
+    displayName: "全機関鉱工業品数",
+  },
+  othersAll: {
+    id: "othersAll",
+    fileName: "othersAll",
+    displayName: "全機関のその他の品数",
+  },
+  specialItemsAll: {
+    id: "specialItemsAll",
+    fileName: "specialItemsAll",
+    displayName: "全機関特殊品数",
+  },
+  totalTrain: {
+    id: "totalTrain",
+    fileName: "totalTrain",
+    displayName: "鉄道総貨物数",
+  },
+  agProdTrain: {
+    id: "agProdTrain",
+    fileName: "agProdTrain",
+    displayName: "鉄道農林水産品数",
+  },
+  chemicalIndustryProductsTrain: {
+    id: "chemicalIndustryProductsTrain",
+    fileName: "chemicalIndustryProductsTrain",
+    displayName: "鉄道化学工業品数",
+  },
+  forestProductsTrain: {
+    id: "forestProductsTrain",
+    fileName: "forestProductsTrain",
+    displayName: "鉄道林産品数",
+  },
+  lightIndustryProductsTrain: {
+    id: "lightIndustryProductsTrain",
+    fileName: "lightIndustryProductsTrain",
+    displayName: "鉄道軽工業品数",
+  },
+  MandMProductsTrain: {
+    id: "MandMProductsTrain",
+    fileName: "MandMProductsTrain",
+    displayName: "鉄道金属機械工業品数",
+  },
+  mineralProductsTrain: {
+    id: "mineralProductsTrain",
+    fileName: "mineralProductsTrain",
+    displayName: "鉄道鉱工業品数",
+  },
+  othersTrain: {
+    id: "othersTrain",
+    fileName: "othersTrain",
+    displayName: "鉄道のその他の品数",
+  },
+  specialItemsTrain: {
+    id: "specialItemsTrain",
+    fileName: "specialItemsTrain",
+    displayName: "鉄道特殊品数",
+  },
+  totalShips: {
+    id: "totalShips",
+    fileName: "totalShips",
+    displayName: "海運総貨物数",
+  },
+  agProdShips: {
+    id: "agProdShips",
+    fileName: "agProdShips",
+    displayName: "海運農林水産品数",
+  },
+  chemicalIndustryProductsShips: {
+    id: "chemicalIndustryProductsShips",
+    fileName: "chemicalIndustryProductsShips",
+    displayName: "海運化学工業品数",
+  },
+  forestProductsShips: {
+    id: "forestProductsShips",
+    fileName: "forestProductsShips",
+    displayName: "海運林産品数",
+  },
+  lightIndustryProductsShips: {
+    id: "lightIndustryProductsShips",
+    fileName: "lightIndustryProductsShips",
+    displayName: "海運軽工業品数",
+  },
+  MandMProductsShips: {
+    id: "MandMProductsShips",
+    fileName: "MandMProductsShips",
+    displayName: "海運金属機械工業品数",
+  },
+  mineralProductsShips: {
+    id: "mineralProductsShips",
+    fileName: "mineralProductsShips",
+    displayName: "海運鉱工業品数",
+  },
+  othersShips: {
+    id: "othersShips",
+    fileName: "othersShips",
+    displayName: "海運のその他の品数",
+  },
+  specialItemsShips: {
+    id: "specialItemsShips",
+    fileName: "specialItemsShips",
+    displayName: "海運特殊品数",
+  },
+  totalCars: {
+    id: "totalCars",
+    fileName: "totalCars",
+    displayName: "自動車総貨物数",
+  },
+  agProdCars: {
+    id: "agProdCars",
+    fileName: "agProdCars",
+    displayName: "自動車農林水産品数",
+  },
+  chemicalIndustryProductsCars: {
+    id: "chemicalIndustryProductsCars",
+    fileName: "chemicalIndustryProductsCars",
+    displayName: "自動車化学工業品数",
+  },
+  forestProductsCars: {
+    id: "forestProductsCars",
+    fileName: "forestProductsCars",
+    displayName: "自動車林産品数",
+  },
+  lightIndustryProductsCars: {
+    id: "lightIndustryProductsCars",
+    fileName: "lightIndustryProductsCars",
+    displayName: "自動車軽工業品数",
+  },
+  MandMProductsCars: {
+    id: "MandMProductsCars",
+    fileName: "MandMProductsCars",
+    displayName: "自動車金属機械工業品数",
+  },
+  mineralProductsCars: {
+    id: "mineralProductsCars",
+    fileName: "mineralProductsCars",
+    displayName: "自動車鉱工業品数",
+  },
+  othersCars: {
+    id: "othersCars",
+    fileName: "othersCars",
+    displayName: "自動車のその他の品数",
+  },
+  specialItemsCars: {
+    id: "specialItemsCars",
+    fileName: "specialItemsCars",
+    displayName: "自動車特殊品数",
+  },
+};
