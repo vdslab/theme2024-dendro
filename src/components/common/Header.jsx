c9719541eb2d0d3752fafd98d64a09d1d8bfc7d9
akimas1846 <akihiro0923092@gmail.com>
2025-02-01 06:14:40 +0900

    タイトル設定と細かい修正その2

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -3,7 +3,7 @@ import { AppBar, Typography } from "@mui/material";
 export const Header = () => {
   return (
     <AppBar position="static" sx={{ paddingX: 1, paddingY: 0.5 }}>
-      <Typography variant="h5">たいとる</Typography>
+      <Typography variant="h5">floria</Typography>
     </AppBar>
   );
 };
